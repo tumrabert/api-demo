64759ec1eeae342ccfc9cedb1f4bc1e28efdc4c2
tumrabert <tanakit.p@ayodiacompany.com>
2024-03-21 11:15:59 +0700

    act-08

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -5,11 +5,12 @@ const {
   getHospitals,
   updateHospital,
   deleteHospital,
+  getVacCenters
 } = require("../controllers/hospitals");
 const router = express.Router();
 const { protect,authorization } = require("../middleware/auth");
 
-
+router.route('/vacCenters').get(getVacCenters);
 router.route("/").get(getHospitals).post(protect,authorization('admin') ,createHospital);
 router.route("/:id").get(getHospital).delete(protect,authorization('admin'), deleteHospital).put(protect,authorization('admin'), updateHospital);
 
